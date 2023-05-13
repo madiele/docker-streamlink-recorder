4f8fda3ca8f96dffefac380db44d96206f1fe6c1
Mattia Di Eleuterio <madiele92@gmail.com>
2023-05-13 19:33:59 +0200

    improve pattern

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -6,10 +6,10 @@ while true; do
   Date=$(date +%Y%m%d-%H%M%S)
   start=$(date +%s)
   filename=/home/download/$STREAM_NAME-$Date.mp4
-  streamlink --twitch-disable-ads "$STREAM_OPTIONS" "$STREAM_LINK" "$STREAM_QUALITY" -o "$filename"
+  streamlink --twitch-disable-ads "$STREAM_OPTIONS" "$STREAM_LINK" "$STREAM_QUALITY" -o "$filename" > /dev/null
   end=$(date +%s)
   elapsed=$((end - start))
   duration=$(printf '%02d:%02d:%02d\n' $((elapsed/3600)) $((elapsed%3600/60)) $((elapsed%60)))
-  mv "$filename" "$STREAM_NAME-$Date-_%_${duration}_%_.mp4"
+  mv "$filename" "$STREAM_NAME-$Date-___${duration}___.mp4"
   sleep 300s
 done
